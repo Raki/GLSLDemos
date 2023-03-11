8f979dceb3844725031971826fc9ac28e8d3524d
Raki <824691+Raki@users.noreply.github.com>
2023-03-10 18:33:11 -0700

    PreMultiplied Alpha blend demo

--- shaders/fBlendPMAlpha.glsl
+++ shaders/fBlendPMAlpha.glsl
@@ -8,19 +8,35 @@ uniform sampler2D sampler2;
 
 uniform float blendFactor;
 
+subroutine vec4 BlendAlphaType(vec4 srcColor,vec4 dstColor);
+subroutine uniform BlendAlphaType BlendAlpha;
+
+subroutine(BlendAlphaType)
+vec4 defaultBlend(vec4 srcColor,vec4 dstColor)
+{
+	return  (1.-srcColor.a)*dstColor+(srcColor.a*srcColor);
+}
+
+subroutine(BlendAlphaType)
+vec4 preMulBlendBasic(vec4 srcColor,vec4 dstColor)
+{
+	return vec4(srcColor.rgb*srcColor.a+dstColor.rgb*dstColor.a,1.);
+}
+
+subroutine(BlendAlphaType)
+vec4 preMulBlend(vec4 srcColor,vec4 dstColor)
+{
+	float final_alpha = srcColor.a + dstColor.a * (1.0 - srcColor.a);
+	return vec4(
+        (srcColor.rgb * srcColor.a + dstColor.rgb * dstColor.a * (1.0 - srcColor.a)) / final_alpha,
+        final_alpha
+    );
+}
+
 void main()
 {
 	vec4 srcColor = texture(sampler1,uvcoord_out);
 	vec4 dstColor = texture(sampler2,uvcoord_out);
 
-	
-	float final_alpha = srcColor.a + dstColor.a * (1.0 - srcColor.a);
-    fragColor = vec4(
-        (srcColor.rgb * srcColor.a + dstColor.rgb * dstColor.a * (1.0 - srcColor.a)) / final_alpha,
-        final_alpha
-    );
-
-	//fragColor = vec4(srcColor.rgb*srcColor.a+dstColor.rgb*dstColor.a,1.);
-
-	//fragColor = (1.-srcColor.a)*dstColor+(srcColor.a*srcColor);
+	fragColor = BlendAlpha(srcColor,dstColor);
 }
